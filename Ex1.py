3835cbf43a4383729a2ed9fbd36f22f1594b25a1
Juliana Barrios <julieth2181558@correo.uis.edu.co>
2021-01-18 17:34:44 -0500

    Mi primer commit de modelado Python

---
+++ Ex1.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import flopy# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
---
+++ Ex1_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+y="Hello :3"
+print(y)
+
+
+# In[5]:
+
+
+x=input("Enter your name:")
+
+
+# In[6]:
+
+
+y="Hello :3"
+x=input("Enter your name:")
+print(y,x)
+
+
+# In[ ]:
+
+
+
+
